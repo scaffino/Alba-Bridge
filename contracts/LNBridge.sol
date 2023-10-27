d157866ac8e8d39c809173bb1cc4c6c532278a88
Giulia Scaffino <gscaffino@Giulias-MacBook-Pro.local>
2023-10-27 10:54:03 +0200

    timelock check done in submitproof, event emitted and tested

--- contracts/LNBridge.sol
+++ contracts/LNBridge.sol
@@ -13,6 +13,8 @@ import "./BTCUtils.sol";
 
 contract LNBridge {
 
+    event stateEvent(string label, bool stateStatus);
+
     // define global variables for this contract instance (setup phase)
     struct BridgeInstance {
         bytes32 fundingTxId;
@@ -22,8 +24,8 @@ contract LNBridge {
         bytes pkProver_Uncompressed; 
         bytes pkVerifier_Uncompressed;
         //uint256 index;
-        //uint timelock; //October 18th, 00.00
-        //uint timelock_dispute; //to be used
+        uint256 timelock; //October 18th, 00.00
+        uint256 timelock_dispute; //to be used
     }
 
     struct BridgeState {
@@ -33,15 +35,23 @@ contract LNBridge {
         bool disputeClosedV;
     }
 
+    struct BalanceDistribution {
+        uint balP;
+        uint balV;
+    }
+
     BridgeInstance public bridge;
     BridgeState public state;
+    BalanceDistribution public balanceDistr;
 
     function setup(bytes32 _fundingTxId, 
                    bytes memory _fundingTx_script, 
                    bytes4 _fundingTx_index, 
                    bytes memory _sighash,
                    bytes memory _pkProver_Uncompressed, 
-                   bytes memory _pkVerifier_Uncompressed) external {
+                   bytes memory _pkVerifier_Uncompressed, 
+                   uint256 _timelock, 
+                   uint256 _timelock_dispute) external {
 
         bridge.fundingTxId = _fundingTxId;
         bridge.fundingTx_script = _fundingTx_script;
@@ -49,6 +59,8 @@ contract LNBridge {
         bridge.sighash = _sighash;
         bridge.pkProver_Uncompressed = _pkProver_Uncompressed;
         bridge.pkVerifier_Uncompressed = _pkVerifier_Uncompressed;
+        bridge.timelock = _timelock;
+        bridge.timelock_dispute = _timelock_dispute;
 
         state.validProofSubmitted = false;
         state.disputeOpened = false;
@@ -58,40 +70,76 @@ contract LNBridge {
 
     function submitProof(bytes memory CT_P_unlocked, 
                          bytes memory CT_V_unlocked) external {
+                        
+        if (block.timestamp < bridge.timelock) {
 
-        // TODO GIULIA: check current time is smaller than timelock defined in setup phase
+            // check transactions are not locked
+            require(ParseBTCLib.getTimelock(CT_P_unlocked) == bytes4(0), "Commitment transaction of P is locked");
+            require(ParseBTCLib.getTimelock(CT_V_unlocked) == bytes4(0), "Commitment transaction of V is locked");
 
-        // check transactions are not locked
-        require(ParseBTCLib.getTimelock(CT_P_unlocked) == bytes4(0), "Commitment transaction of P is locked");
+            // check transactions are well formed
+            ParseBTCLib.LightningHTLCData[2] memory lightningHTLC;
+            ParseBTCLib.P2PKHData[2] memory p2pkh; 
+            ParseBTCLib.OpReturnData memory opreturn;
+            (lightningHTLC[0], p2pkh[0], opreturn) = ParseBTCLib.getOutputsDataLNB(CT_P_unlocked); //note: the p2pkh_P is the p2pkh belonging in P's commitment transaction, but holds the public key of V
+            (lightningHTLC[1], p2pkh[1]) = ParseBTCLib.getOutputsDataLN(CT_V_unlocked); //note: the p2pkh_V is the p2pkh belonging in V's commitment transaction, but holds the public key of P
+
+            require(opreturn.data == lightningHTLC[1].rev_secret, "P's commitment transaction does not hardcode V's revocation key");
+            require(p2pkh[0].value == lightningHTLC[1].value, "Amount mismatch between p2pkh of P and lightning HTLC of V");
+            require(lightningHTLC[0].value == p2pkh[1].value, "Amount mismatch between p2pkh of V and lightning HTLC of P");
+
+            (bytes memory pk1, bytes memory pk2) = ParseBTCLib.extractCompressedPK(bridge.fundingTx_script);
+            require(sha256(BTCUtils.hash160(pk2)) == sha256(abi.encodePacked(p2pkh[0].pkhash)), "The p2pkh in P's unlocked commitment transaction does not correspond to Verifier's one");
+            require(sha256(BTCUtils.hash160(pk1)) == sha256(abi.encodePacked(p2pkh[1].pkhash)), "The p2pkh in V's unlocked commitment transaction does not correspond to Prover's one");        
+
+            // check transactions spend the funding transaction ()
+            require(ParseBTCLib.getInputsData(CT_P_unlocked).txid == bridge.fundingTxId, "P's commitment transaction does not spend the funding transaction");
+            require(ParseBTCLib.getInputsData(CT_V_unlocked).txid == bridge.fundingTxId, "V's commitment transaction does not spend the funding transaction");
+    
+            //retrieve signatures
+            ParseBTCLib.Signature[2] memory sig;
+            sig[1] = ParseBTCLib.getSignature(CT_P_unlocked); // sig V
+            sig[0] = ParseBTCLib.getSignature(CT_V_unlocked); // sig P
+
+            //verify signatures
+            bytes32[2] memory digest;
+            digest[0] =  ParseBTCLib.getTxDigest(CT_P_unlocked, bridge.fundingTx_script, bridge.sighash); // digest of commitment transaction of P 
+            require(sha256(ParseBTCLib.verifyBTCSignature(uint256(digest[0]), uint8(sig[1].v), BytesLib.toUint256(sig[1].r,0), BytesLib.toUint256(sig[1].s,0))) == sha256(bridge.pkVerifier_Uncompressed), "Invalid signature of V over commitment transaction of P"); 
+
+            digest[1] = ParseBTCLib.getTxDigest(CT_V_unlocked, bridge.fundingTx_script, bridge.sighash); // the last argument is the sighash, which in this case is SIGHASH_ALL
+            require(sha256(ParseBTCLib.verifyBTCSignature(uint256(digest[1]), uint8(sig[0].v), BytesLib.toUint256(sig[0].r,0), BytesLib.toUint256(sig[0].s,0))) == sha256(bridge.pkProver_Uncompressed), "Invalid signature of P over commitment transaction of V"); 
+
+            // TODO GIULIA: extract state and save it 
+    
+            state.validProofSubmitted = true;
+
+            emit stateEvent("Proof successfully verified: state.validProofSubmitted = true", state.validProofSubmitted);
+
+        } else {
+
+            emit stateEvent("Proof submitted too late: state.validProofSubmitted = false", state.validProofSubmitted);
+            console.log(state.validProofSubmitted);
+        } 
+    }
+
+    function dispute(bytes memory CT_P_locked, 
+                     bytes memory CT_V_unlocked) external {
+
+        // TODO GIULIA: check current time is larger than timelock defined in setup phase
+
+        // check commitment transaction of P is locked and commitment transaction of V is unlocked
+        require(ParseBTCLib.getTimelock(CT_P_locked) != bytes4(0), "Commitment transaction of P is unlocked"); // TODO GIULIA: need to check until when it is locked
         require(ParseBTCLib.getTimelock(CT_V_unlocked) == bytes4(0), "Commitment transaction of V is locked");
 
-        // check transactions are well formed
-        ParseBTCLib.LightningHTLCData[2] memory lightningHTLC;
-        ParseBTCLib.P2PKHData[2] memory p2pkh; 
-        ParseBTCLib.OpReturnData memory opreturn;
-        (lightningHTLC[0], p2pkh[0], opreturn) = ParseBTCLib.getOutputsDataLNB(CT_P_unlocked); //note: the p2pkh_P is the p2pkh belonging in P's commitment transaction, but holds the public key of V
-        (lightningHTLC[1], p2pkh[1]) = ParseBTCLib.getOutputsDataLN(CT_V_unlocked); //note: the p2pkh_V is the p2pkh belonging in V's commitment transaction, but holds the public key of P
 
-        require(opreturn.data == lightningHTLC[1].rev_secret, "P's commitment transaction does not hardcode V's revocation key");
-        require(p2pkh[0].value == lightningHTLC[1].value, "Amount mismatch between p2pkh of P and lightning HTLC of V");
-        require(lightningHTLC[0].value == p2pkh[1].value, "Amount mismatch between p2pkh of V and lightning HTLC of P");
-
-        (bytes memory pk1, bytes memory pk2) = ParseBTCLib.extractCompressedPK(bridge.fundingTx_script);
-        require(sha256(BTCUtils.hash160(pk2)) == sha256(abi.encodePacked(p2pkh[0].pkhash)), "The p2pkh in P's unlocked commitment transaction does not correspond to Verifier's one");
-        require(sha256(BTCUtils.hash160(pk1)) == sha256(abi.encodePacked(p2pkh[1].pkhash)), "The p2pkh in V's unlocked commitment transaction does not correspond to Prover's one");        
-
-        // check transactions spend the funding transaction ()
-        require(ParseBTCLib.getInputsData(CT_P_unlocked).txid == bridge.fundingTxId, "P's commitment transaction does not spend the funding transaction");
-        require(ParseBTCLib.getInputsData(CT_V_unlocked).txid == bridge.fundingTxId, "V's commitment transaction does not spend the funding transaction");
- 
         //retrieve signatures
         ParseBTCLib.Signature[2] memory sig;
-        sig[1] = ParseBTCLib.getSignature(CT_P_unlocked); // sig V
+        sig[1] = ParseBTCLib.getSignature(CT_P_locked); // sig V
         sig[0] = ParseBTCLib.getSignature(CT_V_unlocked); // sig P
 
         //verify signatures
         bytes32[2] memory digest;
-        digest[0] =  ParseBTCLib.getTxDigest(CT_P_unlocked, bridge.fundingTx_script, bridge.sighash); // digest of commitment transaction of P 
+        digest[0] =  ParseBTCLib.getTxDigest(CT_P_locked, bridge.fundingTx_script, bridge.sighash); // digest of commitment transaction of P 
         require(sha256(ParseBTCLib.verifyBTCSignature(uint256(digest[0]), uint8(sig[1].v), BytesLib.toUint256(sig[1].r,0), BytesLib.toUint256(sig[1].s,0))) == sha256(bridge.pkVerifier_Uncompressed), "Invalid signature of V over commitment transaction of P"); 
 
         digest[1] = ParseBTCLib.getTxDigest(CT_V_unlocked, bridge.fundingTx_script, bridge.sighash); // the last argument is the sighash, which in this case is SIGHASH_ALL
@@ -99,14 +147,7 @@ contract LNBridge {
 
         // TODO GIULIA: extract state and save it 
  
-        state.validProofSubmitted = true;
-
-    }
-
-    function dispute(bytes memory CT_P_locked, 
-                     bytes memory CT_V_unlocked) external {
-
-        // TODO GIULIA: check current time is larger than timelock defined in setup phase
+        state.disputeOpened = true;
 
        
     }
--- test/LNBridge.test.js
+++ test/LNBridge.test.js
@@ -18,7 +18,7 @@ describe("LNBridge", function(account) {
     describe("Call Setup", function () {
 
         it("Populate Setup", async function () {
-            let tx = await LNBridge.setup(testdata.fundingTxId, testdata.fundingTx_LockingScript, testdata.fundingTxIndex, testdata.sighash_all, testdata.pkProverUnprefixedUncompressed, testdata.pkVerifierUnprefixedUncompressed);
+            let tx = await LNBridge.setup(testdata.fundingTxId, testdata.fundingTx_LockingScript, testdata.fundingTxIndex, testdata.sighash_all, testdata.pkProverUnprefixedUncompressed, testdata.pkVerifierUnprefixedUncompressed, testdata.timelock, testdata.RelTimelock);
             const receipt = await ethers.provider.getTransactionReceipt(tx.hash);
         }) 
     });
@@ -27,30 +27,50 @@ describe("LNBridge", function(account) {
 
         it("Is Proof valid?", async function () {
 
-            let txSetup = await LNBridge.setup(testdata.fundingTxId, testdata.fundingTx_LockingScript, testdata.fundingTxIndex, testdata.sighash_all, testdata.pkProverUnprefixedUncompressed, testdata.pkVerifierUnprefixedUncompressed);
+            let txSetup = await LNBridge.setup(testdata.fundingTxId, testdata.fundingTx_LockingScript, testdata.fundingTxIndex, testdata.sighash_all, testdata.pkProverUnprefixedUncompressed, testdata.pkVerifierUnprefixedUncompressed, testdata.timelock, testdata.RelTimelock);
 
             let txSubmitProof = await LNBridge.submitProof(testdata.CT_P_withVsig_Unlocked, testdata.CT_V_withPsig_Unlocked);
 
         }) 
 
+        it("Revert if current time is smaller than the time in the timelock. Event: Proof successfully verified: state.validProofSubmitted = true", async function () {
+
+            let txSetup = await LNBridge.setup(testdata.fundingTxId, testdata.fundingTx_LockingScript, testdata.fundingTxIndex, testdata.sighash_all, testdata.pkProverUnprefixedUncompressed, testdata.pkVerifierUnprefixedUncompressed, testdata.timelock, testdata.RelTimelock);
+
+            await expect(LNBridge.submitProof(testdata.CT_P_withVsig_Unlocked, testdata.CT_V_withPsig_Unlocked)).to.emit(LNBridge, "stateEvent").withArgs("Proof successfully verified: state.validProofSubmitted = true", true);
+        }) 
+
+        it("Revert if current time is smaller than the time in the timelock. Event: Proof submitted too late: state.validProofSubmitted = false", async function () {
+
+            let txSetup = await LNBridge.setup(testdata.fundingTxId, testdata.fundingTx_LockingScript, testdata.fundingTxIndex, testdata.sighash_all, testdata.pkProverUnprefixedUncompressed, testdata.pkVerifierUnprefixedUncompressed, testdata.smallTimelock, testdata.RelTimelock);
+
+            await expect(LNBridge.submitProof(testdata.CT_P_withVsig_Unlocked, testdata.CT_V_withPsig_Unlocked)).to.emit(LNBridge, "stateEvent").withArgs("Proof submitted too late: state.validProofSubmitted = false", false);
+        })
+
         it("Revert if P's transaction is locked", async function () {
+
+            let txSetup = await LNBridge.setup(testdata.fundingTxId, testdata.fundingTx_LockingScript, testdata.fundingTxIndex, testdata.sighash_all, testdata.pkProverUnprefixedUncompressed, testdata.pkVerifierUnprefixedUncompressed, testdata.timelock, testdata.RelTimelock);
 
             await expect(LNBridge.submitProof(testdata.CT_P_withVsig_Locked, testdata.CT_V_withPsig_Unlocked)).to.be.revertedWith("Commitment transaction of P is locked");
         })
         
         it("Revert if V's transaction is locked", async function () {
 
+            let txSetup = await LNBridge.setup(testdata.fundingTxId, testdata.fundingTx_LockingScript, testdata.fundingTxIndex, testdata.sighash_all, testdata.pkProverUnprefixedUncompressed, testdata.pkVerifierUnprefixedUncompressed, testdata.timelock, testdata.RelTimelock);
+
             await expect(LNBridge.submitProof(testdata.CT_P_withVsig_Unlocked, testdata.CT_V_withPsig_Locked)).to.be.revertedWith("Commitment transaction of V is locked");
         })
 
         it("Revert if P's commitment transaction does not hardcode V's revocation key", async function () {
+
+            let txSetup = await LNBridge.setup(testdata.fundingTxId, testdata.fundingTx_LockingScript, testdata.fundingTxIndex, testdata.sighash_all, testdata.pkProverUnprefixedUncompressed, testdata.pkVerifierUnprefixedUncompressed, testdata.timelock, testdata.RelTimelock);
 
             await expect(LNBridge.submitProof(testdata.CT_P_withVsig_Unlocked_WrongRevSecret, testdata.CT_V_withPsig_Unlocked)).to.be.revertedWith("P's commitment transaction does not hardcode V's revocation key");
         })
 
         it("Revert if there is an mismatch between the amounts in p2pkh of P and in lightning HTLC of V (wrong P2PKH of P)", async function () {
 
-            let txSetup = await LNBridge.setup(testdata.fundingTxId, testdata.fundingTx_LockingScript, testdata.fundingTxIndex, testdata.sighash_all, testdata.pkProverUnprefixedUncompressed, testdata.pkVerifierUnprefixedUncompressed);
+            let txSetup = await LNBridge.setup(testdata.fundingTxId, testdata.fundingTx_LockingScript, testdata.fundingTxIndex, testdata.sighash_all, testdata.pkProverUnprefixedUncompressed, testdata.pkVerifierUnprefixedUncompressed, testdata.timelock, testdata.RelTimelock);
 
             await expect(LNBridge.submitProof(testdata.CT_P_withVsig_Unlocked_WrongAmountP2PKH, testdata.CT_V_withPsig_Unlocked)).to.be.revertedWith("Amount mismatch between p2pkh of P and lightning HTLC of V");
 
@@ -58,7 +78,7 @@ describe("LNBridge", function(account) {
 
         it("Revert if there is an mismatch between the amounts in p2pkh of P and in lightning HTLC of V (wrong HTLC of V)", async function () {
 
-            let txSetup = await LNBridge.setup(testdata.fundingTxId, testdata.fundingTx_LockingScript, testdata.fundingTxIndex, testdata.sighash_all, testdata.pkProverUnprefixedUncompressed, testdata.pkVerifierUnprefixedUncompressed);
+            let txSetup = await LNBridge.setup(testdata.fundingTxId, testdata.fundingTx_LockingScript, testdata.fundingTxIndex, testdata.sighash_all, testdata.pkProverUnprefixedUncompressed, testdata.pkVerifierUnprefixedUncompressed, testdata.timelock, testdata.RelTimelock);
 
             await expect(LNBridge.submitProof(testdata.CT_P_withVsig_Unlocked, testdata.CT_V_withPsig_Unlocked_WrongAmountHTLC)).to.be.revertedWith("Amount mismatch between p2pkh of P and lightning HTLC of V");
 
@@ -66,7 +86,7 @@ describe("LNBridge", function(account) {
 
         it("Revert if there is an mismatch between the amounts in p2pkh of P and in lightning HTLC of V", async function () {
 
-            let txSetup = await LNBridge.setup(testdata.fundingTxId, testdata.fundingTx_LockingScript, testdata.fundingTxIndex, testdata.sighash_all, testdata.pkProverUnprefixedUncompressed, testdata.pkVerifierUnprefixedUncompressed);
+            let txSetup = await LNBridge.setup(testdata.fundingTxId, testdata.fundingTx_LockingScript, testdata.fundingTxIndex, testdata.sighash_all, testdata.pkProverUnprefixedUncompressed, testdata.pkVerifierUnprefixedUncompressed, testdata.timelock, testdata.RelTimelock);
 
             await expect(LNBridge.submitProof(testdata.CT_P_withVsig_Unlocked_WrongAmountHTLC, testdata.CT_V_withPsig_Unlocked_WrongAmountP2PKH)).to.be.revertedWith("Amount mismatch between p2pkh of V and lightning HTLC of P");
 
@@ -74,35 +94,35 @@ describe("LNBridge", function(account) {
 
         it("Revert if the p2pkh in P's unlocked commitment transaction does not correspond to Verifier's one", async function () {
 
-            let txSetup = await LNBridge.setup(testdata.fundingTxId, testdata.fundingTx_LockingScript, testdata.fundingTxIndex, testdata.sighash_all, testdata.pkProverUnprefixedUncompressed, testdata.pkVerifierUnprefixedUncompressed);
+            let txSetup = await LNBridge.setup(testdata.fundingTxId, testdata.fundingTx_LockingScript, testdata.fundingTxIndex, testdata.sighash_all, testdata.pkProverUnprefixedUncompressed, testdata.pkVerifierUnprefixedUncompressed, testdata.timelock, testdata.RelTimelock);
 
             await expect(LNBridge.submitProof(testdata.CT_P_withVsig_Unlocked_WrongP2pkh, testdata.CT_V_withPsig_Unlocked)).to.be.revertedWith("The p2pkh in P's unlocked commitment transaction does not correspond to Verifier's one");
         }) 
 
         it("Revert if the p2pkh in V's unlocked commitment transaction does not correspond to Prover's one", async function () {
 
-            let txSetup = await LNBridge.setup(testdata.fundingTxId, testdata.fundingTx_LockingScript, testdata.fundingTxIndex, testdata.sighash_all, testdata.pkProverUnprefixedUncompressed, testdata.pkVerifierUnprefixedUncompressed);
+            let txSetup = await LNBridge.setup(testdata.fundingTxId, testdata.fundingTx_LockingScript, testdata.fundingTxIndex, testdata.sighash_all, testdata.pkProverUnprefixedUncompressed, testdata.pkVerifierUnprefixedUncompressed, testdata.timelock, testdata.RelTimelock);
 
             await expect(LNBridge.submitProof(testdata.CT_P_withVsig_Unlocked, testdata.CT_V_withPsig_Unlocked_WrongP2pkh)).to.be.revertedWith("The p2pkh in V's unlocked commitment transaction does not correspond to Prover's one");
         }) 
 
         it("Revert if P's commitment transaction does not spend the funding transaction", async function () {
 
-            let txSetup = await LNBridge.setup(testdata.fundingTxId, testdata.fundingTx_LockingScript, testdata.fundingTxIndex, testdata.sighash_all, testdata.pkProverUnprefixedUncompressed, testdata.pkVerifierUnprefixedUncompressed);
+            let txSetup = await LNBridge.setup(testdata.fundingTxId, testdata.fundingTx_LockingScript, testdata.fundingTxIndex, testdata.sighash_all, testdata.pkProverUnprefixedUncompressed, testdata.pkVerifierUnprefixedUncompressed, testdata.timelock, testdata.RelTimelock);
 
             await expect(LNBridge.submitProof(testdata.CT_P_withVsig_WrongFund, testdata.CT_V_withPsig_Unlocked)).to.be.revertedWith("P's commitment transaction does not spend the funding transaction");
         })
 
         it("Revert if V's commitment transaction does not spend the funding transaction", async function () {
 
-            let txSetup = await LNBridge.setup(testdata.fundingTxId, testdata.fundingTx_LockingScript, testdata.fundingTxIndex, testdata.sighash_all, testdata.pkProverUnprefixedUncompressed, testdata.pkVerifierUnprefixedUncompressed);
+            let txSetup = await LNBridge.setup(testdata.fundingTxId, testdata.fundingTx_LockingScript, testdata.fundingTxIndex, testdata.sighash_all, testdata.pkProverUnprefixedUncompressed, testdata.pkVerifierUnprefixedUncompressed, testdata.timelock, testdata.RelTimelock);
 
             await expect(LNBridge.submitProof(testdata.CT_P_withVsig_Unlocked, testdata.CT_V_withPsig_WrongFund)).to.be.revertedWith("V's commitment transaction does not spend the funding transaction");
         })
 
         it("Revert if verification of signature of P failed", async function () {
 
-            let txSetup = await LNBridge.setup(testdata.fundingTxId, testdata.fundingTx_LockingScript, testdata.fundingTxIndex, testdata.sighash_all, testdata.pkProverUnprefixedUncompressed, testdata.pkVerifierUnprefixedUncompressed);
+            let txSetup = await LNBridge.setup(testdata.fundingTxId, testdata.fundingTx_LockingScript, testdata.fundingTxIndex, testdata.sighash_all, testdata.pkProverUnprefixedUncompressed, testdata.pkVerifierUnprefixedUncompressed, testdata.timelock, testdata.RelTimelock);
 
             await expect(LNBridge.submitProof(testdata.CT_P_withVsig_Unlocked, testdata.CT_V_withWrongPsig_Unlocked)).to.be.revertedWith("Invalid signature of P over commitment transaction of V");
 
@@ -110,12 +130,20 @@ describe("LNBridge", function(account) {
 
         it("Revert if verification of signature of V failed", async function () {
 
-            let txSetup = await LNBridge.setup(testdata.fundingTxId, testdata.fundingTx_LockingScript, testdata.fundingTxIndex, testdata.sighash_all, testdata.pkProverUnprefixedUncompressed, testdata.pkVerifierUnprefixedUncompressed);
+            let txSetup = await LNBridge.setup(testdata.fundingTxId, testdata.fundingTx_LockingScript, testdata.fundingTxIndex, testdata.sighash_all, testdata.pkProverUnprefixedUncompressed, testdata.pkVerifierUnprefixedUncompressed, testdata.timelock, testdata.RelTimelock);
 
             await expect(LNBridge.submitProof(testdata.CT_P_withWrongVsig_Unlocked, testdata.CT_V_withPsig_Unlocked)).to.be.revertedWith("Invalid signature of V over commitment transaction of P");
 
         }) 
 
     });
+
+    /* describe("Test Dispute", function () {
+
+        it("Call Dispute", async function () {
+            let tx = await LNBridge.dispute(testdata.CT_P_withVsig_Locked, testdata.CT_P_withVsig_Unlocked);
+            const receipt = await ethers.provider.getTransactionReceipt(tx.hash);
+        }) 
+    }); */
 
 })
